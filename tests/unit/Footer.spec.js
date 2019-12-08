cd9d7508ca82f1248fb13fb1569de69837ba82fa
janerikkonksi <jan.konksi@mail.ee>
2019-12-08 12:50:01 +0200

    finished 3rd

--- tests/unit/Footer.spec.js
+++ tests/unit/Footer.spec.js
@@ -1,20 +1,18 @@
 import { mount } from '@vue/test-utils'
 import Footer from "../../src/components/Footer";
-import sinon from 'sinon'
 
 describe("Footer component", ()=> {
+    const wrapper = mount(Footer, {
+        propsData: {
+            open: false
+        }
+    });
 
     it('Click on + button changes "open" property to true', () => {
-        const spy = sinon.spy();
-        const wrapper = mount(Footer, {
-            propsData: {
-                callMe: spy
-            }
-        });
         const plusButton = wrapper.find('#plusButton');
-
         plusButton.trigger('click');
-        expect(wrapper.find('#plusButton').exists()).toBe(true);
+        expect(wrapper.vm.$data.open).toBe(true);
+
     })
 });
 
