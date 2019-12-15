002b6bd24e34d6c3f454387add9dc6f8bff8e175
Rudolf Stenar Saluoks <rudolf.saluoks@gmail.com>
2019-12-15 16:55:03 +0200

    Task 4. Made a test to test if task is updated to done after clicking the done button.

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -3,18 +3,32 @@ import List from "../../src/components/List";
 
 describe('List component', () => {
 
-    const emptyList = [];
-    const wrapper = mount(List, {
-        propsData: {
-            list: emptyList
-        }
-    });
-
     it('displays correct message when no items are in list', () => {
+        const emptyList = [];
+        const wrapper = mount(List, {
+            propsData: {
+                list: emptyList
+            }
+        });
         const items = wrapper.findAll('.list-item');
         const message = "Add your first Todo task";
         if(items.length === emptyList.length)
             expect(wrapper.html()).toContain(message)
     });
 
+    it('todo task is updated to done when clicking done button', () => {
+        const madeupList = [{
+            index: 0,
+            title: "Make your bed",
+            done: 0
+        }];
+        const wrapper = mount(List, {
+            propsData: {
+                list: madeupList
+            }
+        });
+        wrapper.find('div.list-item span').trigger('click');
+        const items = wrapper.findAll('.list-item.done');
+        expect(wrapper.props('list')[0]['done']).toBe(true);
+    });
 });
